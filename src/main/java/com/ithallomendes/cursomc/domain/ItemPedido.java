ed0ce57e7fd497c7b48cd70506f9d54c447580cf
Ithallo Mendes Valeriano <it_valeriano>
2022-06-01 17:32:58 -0300

    Item Pedido e e Item PedidoPK

---
+++ src/main/java/com/ithallomendes/cursomc/domain/ItemPedido.java
@@ -0,0 +1,40 @@
+package com.ithallomendes.cursomc.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class ItemPedido implements Serializable {
+    @EmbeddedId
+    private ItemPedidoPK id = new ItemPedidoPK();
+
+    private Double desconto;
+    private Integer quantidade;
+    private Double preco;
+
+    public ItemPedido(){}
+
+    public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
+        id.setPedido(pedido);
+        id.setProduto(produto);
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public Pedido getPedido(){
+        return id.getPedido();
+    }
+
+    public Produto getProduto(){
+        return id.getProduto();
+    }
+
+
+}
--- src/main/java/com/ithallomendes/cursomc/domain/Produto.java
+++ src/main/java/com/ithallomendes/cursomc/domain/Produto.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 public class Produto implements Serializable {
@@ -32,6 +30,18 @@ public class Produto implements Serializable {
         this.nome = nome;
         this.valor = valor;
     }
+
+    public List<Pedido> getPedidos(){
+        List<Pedido> lista = new ArrayList<>();
+        for (ItemPedido x : itens){
+            lista.add(x.getPedido());
+        }
+        return lista;
+    }
+
+    @OneToMany(mappedBy = "id.produto")
+    private Set<ItemPedido> itens = new HashSet<>();
+
 
     public Integer getId() {
         return id;
@@ -63,6 +73,14 @@ public class Produto implements Serializable {
 
     public void setCategorias(List<Categoria> categorias) {
         this.categorias = categorias;
+    }
+
+    public Set<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(Set<ItemPedido> itens) {
+        this.itens = itens;
     }
 
     @Override
